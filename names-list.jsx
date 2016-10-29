898fa1d1af4a0b0d5b05218c8349038ee6882360
kwhitejr <kwhitejr.edu@gmail.com>
2016-10-28 19:17:33 -1000

    created add and list components

---
+++ names-list.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import model from './model.js'
+
+class NamesList extends React.Component {
+  constructor() {
+    super()
+    this.state = {names: {}}
+  }
+
+  componentWillMount() {
+    this.update()
+  }
+
+  update() {
+    model.getValue(['names', 'length'])
+      .then(length => model.get(['names', {from: 0, to: length-1}]))
+      .then(response => this.setState({names: response.json.names}))
+  }
+
+  render() {
+    const names = Object.keys(this.state.names).map(idx => {
+      return <li key={idx}>{this.state.names[idx].name}</li>
+    })
+  }
+}
