ea7f518e393e4dd02b9bb2763a2b7798935a8261
kwhitejr <kwhitejr.edu@gmail.com>
2016-10-28 18:48:56 -1000

    created falcor model

---
+++ model.js
@@ -0,0 +1,8 @@
+import Falcor from 'falcor'
+import FalcorDataSource from 'falcor-http-datasource'
+
+const model = new Falcor.Model({
+  source: new FalcorDataSource('/model.json')
+})
+
+module.exports = model
